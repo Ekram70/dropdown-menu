9a0f253d5194201747209c79fadb510b241fb641
Ekram Ullah Dewan <ekramullah70@gmail.com>
2021-06-26 16:09:37 +0600

    sceleton done

---
+++ main.js
@@ -0,0 +1,9 @@
+let dropdownListItem = document.querySelectorAll(".dropdown-list-item");
+console.log(dropdownListItem);
+
+let dropdownSubmenuItem = document.querySelectorAll(".dropdown-submenu-item");
+console.log(dropdownSubmenuItem);
+
+for (let x = 0; x < dropdownListItem.length; x++) {
+  dropdownListItem[0].addEventListener("click", function () {});
+}
