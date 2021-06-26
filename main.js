8c6b44838fa039ba1602eb8f52a15370cbf0fb8d
Ekram70 <ekramullah70@gmail.com>
2021-06-26 18:58:32 +0600

    need to fix gap problem while hovering

--- main.js
+++
@@ -1,9 +0,0 @@
-let dropdownListItem = document.querySelectorAll(".dropdown-list-item");
-console.log(dropdownListItem);
-
-let dropdownSubmenuItem = document.querySelectorAll(".dropdown-submenu-item");
-console.log(dropdownSubmenuItem);
-
-for (let x = 0; x < dropdownListItem.length; x++) {
-  dropdownListItem[0].addEventListener("click", function () {});
-}
